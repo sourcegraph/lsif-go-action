9873cbc86dcffd42ec78ae55ef9374a8aef7a888
Eric Fritz <eric@eric-fritz.com>
2019-10-09 10:45:11 -0500

    Add debug output when verbose.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -z "$VERBOSE" ]; then
-    lsif-go --noContents --out temp.lsif -v
+    lsif-go --noContents --out temp.lsif -v --debug
     lsif-gomod --in temp.lsif --out data.lsif
     rm temp.lsif
 else
