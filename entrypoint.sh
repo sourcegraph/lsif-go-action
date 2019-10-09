8c269d00554306198a23a525be7b146700c15a64
Eric Fritz <eric@eric-fritz.com>
2019-10-09 09:23:54 -0500

    Add verbose flag.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-lsif-go --noContents --stdout | lsif-gomod --stdin --out data.lsif
+if [ ! -z "$VERBOSE" ]; then
+    lsif-go --noContents --out temp.lsif -v
+    lsif-gomod --in temp.lsif --out data.lsif
+    rm temp.lsif
+else
+    lsif-go --noContents --stdout | lsif-gomod --stdin --out data.lsif
+fi
