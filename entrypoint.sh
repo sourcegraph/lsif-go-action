69585dd5ea4430abfe9bb38c791a9f9178f37189
TJ DeVries <devries.timothyj@gmail.com>
2021-11-29 13:45:30 -0500

    feat: Allow passing additional args to lsif-go (#7)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 : ${OUT:?output file not specified}
 : ${PROJECT_ROOT:?project root not specified}
 : ${MODULE_ROOT:?module root not specified}
+: ${ADDITIONAL_ARGS=''}
 
 OUTPUT_DIR="$(cd "$(dirname "$OUT")" && pwd)"
 ABS_OUT="${OUTPUT_DIR}/$(basename "$OUT")"
 cd "${PROJECT_ROOT}"
-lsif-go --out "$ABS_OUT" --moduleRoot "$MODULE_ROOT"
+lsif-go --output "$ABS_OUT" --module-root "$MODULE_ROOT" $ADDITIONAL_ARGS
 cd -
